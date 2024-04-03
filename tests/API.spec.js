a194c3386019ccc05d77f46061c1997fd669ecc8
talyseugenio <talysebf@gmail.com>
2024-04-03 14:18:42 -0300

    Fix API

--- tests/API.spec.js
+++ tests/API.spec.js
@@ -1,37 +1,19 @@
-import {test, expect} from '@playwright/test';
+import { test, expect, beforeEach } from '@playwright/test';
 const faker = require('faker');
 
 function generateRandomAdm() {
-    const genders = [true, false];
-    return faker.random.arrayElement(genders);
-  }
-
-// Função para obter o ID do último usuário
-async function getLastUserId(request) {
-    const response = await request.get(`${apiBaseUrl}`);
-    expect(response.status()).toBe(200);
-
-    const users = await response.json();
-    
-    // Verifica se a lista de usuários não está vazia
-    if (users.length > 0) {
-        const lastUserIndex = users.length - 1; // Índice do último usuário na lista
-        lastUserId = users[lastUserIndex]._id; // Extrai o ID do último usuário
-
-        console.log("ID do último usuário:", lastUserId);
-    } else {
-        console.log("Não há usuários na lista.");
-    }
+    const admin = [true, false];
+    return faker.random.arrayElement(admin);
 }
 
 const firstName = faker.name.firstName();
+const midleName = faker.name.firstName();
 const lastName = faker.name.lastName();
-const fullName = firstName + " " + lastName
+const fullName = firstName + " " + lastName;
 const email = faker.internet.email();
 const password = faker.internet.password();
 const adm = generateRandomAdm().toString();
-let lastUserId; 
-
+let userId;
 
 // Variável para URLs da API
 const apiBaseUrl = 'https://serverest.dev/usuarios';
@@ -46,67 +28,68 @@ test('Api POST request', async ({ request }) => {
         }
     });
 
-    expect(response.status()).toBe(201); // Validação esperada - "Cadastro realizado com sucesso"
+    await expect(response.status()).toBe(201); // Validação esperada - "Cadastro realizado com sucesso"
+
+    const responseData = await response.json();
+    await expect(responseData.message).toContain('Cadastro realizado com sucesso');
+
+    userId = responseData._id; // Armazena o ID do usuário criado
+
+    console.log(responseData);
+});
+
+test('Api PUT request', async ({ request }) => {
+    const postResponse = await request.post(`${apiBaseUrl}`, {
+        data: {
+            "nome": fullName + midleName,
+            "email": email + ".us",
+            "password": password,
+            "administrador": adm
+        }
+    });
+
+    const postReturn = await postResponse.json()
+    const user = postReturn._id
+
+    const response = await request.put(`${apiBaseUrl}/${user}`, {
+        data: {
+            "nome": fullName + " Talys",
+            "email": email + ".br",
+            "password": password + "123",
+            "administrador": adm
+        }
+    });
+
+    await expect(response.status()).toBe(200);
 
     const text = await response.text();
-    expect(text).toContain('Cadastro realizado com sucesso');
+    await expect(text).toContain('Registro alterado com sucesso');
 
     console.log(await response.json());
 });
 
-test('Api PUT request using lastUserId', async ({ request }) => {
-    // Garante que lastUserId tenha sido definido corretamente
-    if (lastUserId !== undefined) {
-        const response = await request.put(`${apiBaseUrl}/${lastUserId}`, {
-            data: {
-                // Seus dados para atualização aqui
-            }
-        });
-
-        expect(response.status()).toBe(200);
-
-        const text = await response.text();
-        expect(text).toContain('Registro alterado com sucesso');
-
-        console.log(await response.json());
-    }
-});
-
-// Teste para obter o ID do último usuário
+// Teste para obter os dados do usuário criado
 test('Api GET Request id', async ({ request }) => {
-    // Chama a função para obter o ID do último usuário antes de fazer a requisição GET
-    await getLastUserId(request);
+    const response = await request.get(`${apiBaseUrl}/${userId}`);
 
-    // Garante que lastUserId tenha sido definido corretamente
-    if (lastUserId !== undefined) {
-        const response = await request.get(`${apiBaseUrl}/${lastUserId}`);
+    // Verifica se o status da resposta é 200
+    await expect(response.status()).toBe(200);
 
-        // Verifica se o status da resposta é 200
-        expect(response.status()).toBe(200);
+    // Extrai o texto da resposta para verificar se o nome do usuário está presente
+    const text = await response.text();
+    await expect(text).toContain(fullName);
 
-        // Extrai o texto da resposta para verificar se o nome do usuário está presente
-        const text = await response.text();
-        expect(text).toContain(fullName);
-
-        console.log(await response.json());
-    } else {
-        console.log("lastUserId é undefined. Verifique o teste 'Api GET Request id'.");
-    }
+    console.log(await response.json());
 });
 
 // Teste para exclusão de um usuário específico por ID
-test('Api DELETE request', async({request}) => {
-    // Garante que lastUserId tenha sido definido corretamente
-    if (lastUserId !== undefined) {
-        const response = await request.delete(`${apiBaseUrl}/${lastUserId}`);
+test('Api DELETE request', async ({ request }) => {
+    const response = await request.delete(`${apiBaseUrl}/${userId}`);
 
-        // Verifica se o status da resposta é 200
-        await expect(response.status()).toBe(200);
+    // Verifica se o status da resposta é 200
+    await expect(response.status()).toBe(200);
 
-        // Verifica se a mensagem de exclusão está presente no corpo da resposta
-        const text = await response.text();
-        await expect(text).toContain('Registro excluído com sucesso | Nenhum registro excluído');
-    } else {
-        console.log("lastUserId é undefined. Verifique o teste 'Api GET Request id'.");
-    }
+    // Verifica se a mensagem de exclusão está presente no corpo da resposta
+    const text = await response.text();
+    await expect(text).toContain('Registro excluído com sucesso');
 });
